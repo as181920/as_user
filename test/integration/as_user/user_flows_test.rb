d77676b29d99dcf1b1b187021795a14dd59c1dcc
Andersen Fan <as181920@hotmail.com>
2012-12-18 19:03:32 +0800

    add integration for user flows

---
+++ test/integration/as_user/user_flows_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UserFlowsTest < ActionDispatch::IntegrationTest
+  test "create user and browe home and finally signout and signin again" do
+    #get new_user_path
+    get "/as_user/users/new"
+    assert_response :success
+
+    post_via_redirect "/as_user/users", user: {email: "dummy@domain.com", name: "dummy", password: "dummy", password_confirmation: "dummy"}
+    loged_in_path = path
+    assert path =~ /as_user\/users\/\d+/
+    assert_equal "User was successfully created.", flash[:notice]
+
+    get "/as_user"
+    assert_response :success
+
+    get_via_redirect "/as_user/signin"
+    assert_equal loged_in_path, path
+
+    delete_via_redirect "/as_user/signout"
+    assert_equal "/as_user/", path
+    get_via_redirect "/as_user/signin"
+    assert_equal "/as_user/signin", path
+
+    post_via_redirect "/as_user/sessions", session: {email: "dummy", password: "dummy"}
+    assert_template :new
+    assert_equal "Invalid email/password combination", flash[:error]
+    post_via_redirect "/as_user/sessions", session: {email: "dummy@domain.com", password: "dummy"}
+    assert_equal loged_in_path, path
+
+    get_via_redirect "/as_user/signin"
+    assert_equal loged_in_path, path
+  end
+end
